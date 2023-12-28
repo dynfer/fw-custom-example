2b0011c6124fa4bb3003292da0b3ee721cb285ae
GitHub git update Action <action@github.com>
2023-12-28 09:39:55 +0000

    GHA Automation

--- generated/controllers/generated/rusefi_generated_scg-ecu2.0.h
+++ generated/controllers/generated/rusefi_generated_scg-ecu2.0.h
@@ -1442,7 +1442,7 @@
 #define ts_show_vr_threshold_2 true
 #define ts_show_vr_threshold_all true
 #define ts_show_vr_threshold_pins true
-#define TS_SIGNATURE "rusEFI default.2023.12.27.scg-ecu2.0.3955676943"
+#define TS_SIGNATURE "rusEFI default.2023.12.28.scg-ecu2.0.3955676943"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
--- generated/controllers/generated/signature_scg-ecu2.0.h
+++ generated/controllers/generated/signature_scg-ecu2.0.h
@@ -3,4 +3,4 @@
 //
 
 #define SIGNATURE_HASH 3955676943
-#define TS_SIGNATURE "rusEFI default.2023.12.27.scg-ecu2.0.3955676943"
+#define TS_SIGNATURE "rusEFI default.2023.12.28.scg-ecu2.0.3955676943"
